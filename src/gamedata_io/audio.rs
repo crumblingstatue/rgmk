58f77f481f4518b5274cc0597aa8d501354854b4
Mika Attila <radiantstatue@gmail.com>
2015-10-28 15:51:24 +0100

    Move Chunk implementation for Audio into its own module

---
+++ src/gamedata_io/audio.rs
@@ -0,0 +1,60 @@
+use std::io::prelude::*;
+use std::io;
+use byteorder::{ReadBytesExt, WriteBytesExt, LittleEndian};
+use {GameDataRead, GameDataWrite, Audio, AudioData};
+use gamedata_io::{Chunk, get_chunk_header, ReadError, read_into_byte_vec};
+
+impl<'a> Chunk<'a> for Audio {
+    const TYPE_ID: &'static [u8; 4] = b"AUDO";
+    fn read<R: GameDataRead>(reader: &mut R) -> Result<Self::ReadOutput, ReadError> {
+        let header = try!(get_chunk_header(reader, Self::TYPE_ID));
+        let num_audio = try!(reader.read_u32::<LittleEndian>());
+        trace!("num audio: {}", num_audio);
+        // Get offsets
+        let mut offsets = Vec::new();
+        let audio_data_offset = try!(reader.seek(io::SeekFrom::Current(0))) as u32 +
+                                (num_audio * 4);
+        trace!("audio_data_offset is {} ", audio_data_offset);
+        for _ in 0..num_audio {
+            offsets.push(try!(reader.read_u32::<LittleEndian>()) - audio_data_offset);
+        }
+        // Get audio
+        let mut audio = Vec::new();
+
+        for (i, &offset) in offsets.iter().enumerate() {
+            let offset =
+                try!(reader.seek(io::SeekFrom::Start((audio_data_offset + offset) as u64)));
+            let size = try!(reader.read_u32::<LittleEndian>());
+            trace!("({}) @offset {} Reading audio file of size {}",
+                   i,
+                   offset,
+                   size);
+            audio.push(AudioData { data: try!(read_into_byte_vec(reader, size as usize)) });
+        }
+        Ok(Audio {
+            audio: audio,
+            offsets: offsets,
+            size: header.size as u32,
+        })
+    }
+    fn write<W: GameDataWrite>(&self, writer: &mut W, _: ()) -> io::Result<()> {
+        try!(writer.write_all(Self::TYPE_ID));
+        try!(writer.write_u32::<LittleEndian>(self.content_size()));
+        try!(writer.write_u32::<LittleEndian>(self.audio.len() as u32));
+        let audio_data_offset = try!(writer.seek(io::SeekFrom::Current(0))) as u32 +
+                                (self.offsets.len() as u32 * 4);
+        for &offset in &self.offsets {
+            trace!("Writing offset {} ", audio_data_offset + offset);
+            try!(writer.write_u32::<LittleEndian>(audio_data_offset + offset));
+        }
+        for (&offset, data) in self.offsets.iter().zip(self.audio.iter()) {
+            try!(writer.seek(io::SeekFrom::Start((audio_data_offset + offset) as u64)));
+            try!(writer.write_u32::<LittleEndian>(data.data.len() as u32));
+            try!(writer.write_all(&data.data));
+        }
+        Ok(())
+    }
+    fn content_size(&self) -> u32 {
+        self.size
+    }
+}
--- src/gamedata_io/mod.rs
+++ src/gamedata_io/mod.rs
@@ -5,7 +5,7 @@ use std::io;
 use byteorder::{self, ReadBytesExt, WriteBytesExt, LittleEndian};
 use super::{GameData, MetaData, Options, Extn, Sounds, AudioGroups, Sprites, Backgrounds,
             Paths, Scripts, Shaders, Fonts, Timelines, Objects, Rooms, Dafl, Tpag, Code,
-            Variables, Functions, Strings, Textures, AudioData,
+            Variables, Functions, Strings, Textures,
             Audio, GameDataRead, GameDataWrite};
 
 mod meta_data;
@@ -16,6 +16,7 @@ mod variables;
 mod functions;
 mod strings;
 mod textures;
+mod audio;
 
 pub use self::strings::StringReadError;
 
@@ -325,61 +326,6 @@ unk_chunk!(Rooms, b"ROOM");
 unk_chunk!(Dafl, b"DAFL");
 unk_chunk!(Tpag, b"TPAG");
 unk_chunk!(Code, b"CODE");
-
-impl<'a> Chunk<'a> for Audio {
-    const TYPE_ID: &'static [u8; 4] = b"AUDO";
-    fn read<R: GameDataRead>(reader: &mut R) -> Result<Self::ReadOutput, ReadError> {
-        let header = try!(get_chunk_header(reader, Self::TYPE_ID));
-        let num_audio = try!(reader.read_u32::<LittleEndian>());
-        trace!("num audio: {}", num_audio);
-        // Get offsets
-        let mut offsets = Vec::new();
-        let audio_data_offset = try!(reader.seek(io::SeekFrom::Current(0))) as u32 +
-                                (num_audio * 4);
-        trace!("audio_data_offset is {} ", audio_data_offset);
-        for _ in 0..num_audio {
-            offsets.push(try!(reader.read_u32::<LittleEndian>()) - audio_data_offset);
-        }
-        // Get audio
-        let mut audio = Vec::new();
-
-        for (i, &offset) in offsets.iter().enumerate() {
-            let offset =
-                try!(reader.seek(io::SeekFrom::Start((audio_data_offset + offset) as u64)));
-            let size = try!(reader.read_u32::<LittleEndian>());
-            trace!("({}) @offset {} Reading audio file of size {}",
-                   i,
-                   offset,
-                   size);
-            audio.push(AudioData { data: try!(read_into_byte_vec(reader, size as usize)) });
-        }
-        Ok(Audio {
-            audio: audio,
-            offsets: offsets,
-            size: header.size as u32,
-        })
-    }
-    fn write<W: GameDataWrite>(&self, writer: &mut W, _: ()) -> io::Result<()> {
-        try!(writer.write_all(Self::TYPE_ID));
-        try!(writer.write_u32::<LittleEndian>(self.content_size()));
-        try!(writer.write_u32::<LittleEndian>(self.audio.len() as u32));
-        let audio_data_offset = try!(writer.seek(io::SeekFrom::Current(0))) as u32 +
-                                (self.offsets.len() as u32 * 4);
-        for &offset in &self.offsets {
-            trace!("Writing offset {} ", audio_data_offset + offset);
-            try!(writer.write_u32::<LittleEndian>(audio_data_offset + offset));
-        }
-        for (&offset, data) in self.offsets.iter().zip(self.audio.iter()) {
-            try!(writer.seek(io::SeekFrom::Start((audio_data_offset + offset) as u64)));
-            try!(writer.write_u32::<LittleEndian>(data.data.len() as u32));
-            try!(writer.write_all(&data.data));
-        }
-        Ok(())
-    }
-    fn content_size(&self) -> u32 {
-        self.size
-    }
-}
 
 fn texture_data_offset(textures: &Textures, base_offset: u32) -> u32 {
     let mut offset = base_offset + CHUNK_HEADER_LEN;
