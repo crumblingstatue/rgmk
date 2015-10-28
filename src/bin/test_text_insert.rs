7c00a9653c422f4bdb9ca02f5e548cf018ae0fb6
Mika Attila <radiantstatue@gmail.com>
2015-10-28 14:47:32 +0100

    Update test_text_insert

--- src/bin/test_text_insert.rs
+++ src/bin/test_text_insert.rs
@@ -5,9 +5,6 @@ fn main() {
     env_logger::init().unwrap();
     let path = std::env::args().skip(1).next().expect("Expected file name as argument");
     let mut gamedata = rgmk::GameData::from_file(&path).unwrap();
-    let mut string = gamedata.string_at(0).to_owned();
-    string.push('K');
-    println!("\"{}\"", string);
-    gamedata.replace_string_at(0, string);
+    gamedata.strings.strings[0].push('K');
     gamedata.save_to_file(&(path + ".rgmk")).unwrap();
 }
