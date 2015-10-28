62cbe991057fa605e8ba0702f1eff08167578db7
Mika Attila <radiantstatue@gmail.com>
2015-10-28 15:35:58 +0100

    Move the Chunk implementation for Scripts into its own module

--- src/gamedata_io/mod.rs
+++ src/gamedata_io/mod.rs
@@ -4,13 +4,14 @@ use std::io::prelude::*;
 use std::io;
 use byteorder::{self, ReadBytesExt, WriteBytesExt, LittleEndian};
 use super::{GameData, MetaData, Options, Extn, Sounds, AudioGroups, Sprites, Backgrounds,
-            Paths, Script, Scripts, Shaders, Fonts, Timelines, Objects, Rooms, Dafl, Tpag, Code,
+            Paths, Scripts, Shaders, Fonts, Timelines, Objects, Rooms, Dafl, Tpag, Code,
             Variable, Variables, Function, Functions, Strings, Texture, Textures, AudioData,
             Audio, GameDataRead, GameDataWrite};
 
 mod meta_data;
 mod options;
 mod sounds;
+mod scripts;
 
 quick_error! {
     #[derive(Debug)]
@@ -335,52 +336,6 @@ unk_chunk!(Rooms, b"ROOM");
 unk_chunk!(Dafl, b"DAFL");
 unk_chunk!(Tpag, b"TPAG");
 unk_chunk!(Code, b"CODE");
-
-impl<'a> Chunk<'a> for Scripts {
-    const TYPE_ID: &'static [u8; 4] = b"SCPT";
-    type ReadOutput = (Self, Vec<u32>);
-    type WriteInput = &'a [u32];
-    fn read<R: GameDataRead>(reader: &mut R) -> Result<Self::ReadOutput, ReadError> {
-        try!(get_chunk_header(reader, Self::TYPE_ID));
-        let num_scripts = try!(reader.read_u32::<LittleEndian>());
-        trace!("{} scripts", num_scripts);
-        // Read script entry offsets
-        for _ in 0..num_scripts {
-            // For now just discard them
-            try!(reader.read_u32::<LittleEndian>());
-        }
-        let mut name_offsets = Vec::new();
-        let mut scripts = Vec::new();
-        for _ in 0..num_scripts {
-            name_offsets.push(try!(reader.read_u32::<LittleEndian>()));
-            scripts.push(Script {
-                name_index: 0,
-                unknown: try!(reader.read_u32::<LittleEndian>()),
-            });
-        }
-        Ok((Scripts { scripts: scripts }, name_offsets))
-    }
-    fn write<W: GameDataWrite>(&self, writer: &mut W, input: Self::WriteInput) -> io::Result<()> {
-        try!(writer.write_all(Self::TYPE_ID));
-        try!(writer.write_u32::<LittleEndian>(self.content_size()));
-        try!(writer.write_u32::<LittleEndian>(self.scripts.len() as u32));
-        let writer_offset = try!(writer.seek(io::SeekFrom::Current(0))) as u32;
-        let first_script_offset = writer_offset + (self.scripts.len() as u32 * 4);
-        // Write offset data
-        for i in 0..self.scripts.len() as u32 {
-            try!(writer.write_u32::<LittleEndian>(first_script_offset + (i * 8)));
-        }
-        // Write script data
-        for s in &self.scripts {
-            try!(writer.write_u32::<LittleEndian>(input[s.name_index]));
-            try!(writer.write_u32::<LittleEndian>(s.unknown));
-        }
-        Ok(())
-    }
-    fn content_size(&self) -> u32 {
-        4 + (self.scripts.len() as u32 * (4 + (2 * 4)))
-    }
-}
 
 impl<'a> Chunk<'a> for Variables {
     const TYPE_ID: &'static [u8; 4] = b"VARI";
---
+++ src/gamedata_io/scripts.rs
@@ -0,0 +1,51 @@
+use std::io::prelude::*;
+use std::io;
+use byteorder::{ReadBytesExt, WriteBytesExt, LittleEndian};
+use {GameDataRead, GameDataWrite, Script, Scripts};
+use gamedata_io::{Chunk, get_chunk_header, ReadError};
+
+impl<'a> Chunk<'a> for Scripts {
+    const TYPE_ID: &'static [u8; 4] = b"SCPT";
+    type ReadOutput = (Self, Vec<u32>);
+    type WriteInput = &'a [u32];
+    fn read<R: GameDataRead>(reader: &mut R) -> Result<Self::ReadOutput, ReadError> {
+        try!(get_chunk_header(reader, Self::TYPE_ID));
+        let num_scripts = try!(reader.read_u32::<LittleEndian>());
+        trace!("{} scripts", num_scripts);
+        // Read script entry offsets
+        for _ in 0..num_scripts {
+            // For now just discard them
+            try!(reader.read_u32::<LittleEndian>());
+        }
+        let mut name_offsets = Vec::new();
+        let mut scripts = Vec::new();
+        for _ in 0..num_scripts {
+            name_offsets.push(try!(reader.read_u32::<LittleEndian>()));
+            scripts.push(Script {
+                name_index: 0,
+                unknown: try!(reader.read_u32::<LittleEndian>()),
+            });
+        }
+        Ok((Scripts { scripts: scripts }, name_offsets))
+    }
+    fn write<W: GameDataWrite>(&self, writer: &mut W, input: Self::WriteInput) -> io::Result<()> {
+        try!(writer.write_all(Self::TYPE_ID));
+        try!(writer.write_u32::<LittleEndian>(self.content_size()));
+        try!(writer.write_u32::<LittleEndian>(self.scripts.len() as u32));
+        let writer_offset = try!(writer.seek(io::SeekFrom::Current(0))) as u32;
+        let first_script_offset = writer_offset + (self.scripts.len() as u32 * 4);
+        // Write offset data
+        for i in 0..self.scripts.len() as u32 {
+            try!(writer.write_u32::<LittleEndian>(first_script_offset + (i * 8)));
+        }
+        // Write script data
+        for s in &self.scripts {
+            try!(writer.write_u32::<LittleEndian>(input[s.name_index]));
+            try!(writer.write_u32::<LittleEndian>(s.unknown));
+        }
+        Ok(())
+    }
+    fn content_size(&self) -> u32 {
+        4 + (self.scripts.len() as u32 * (4 + (2 * 4)))
+    }
+}
