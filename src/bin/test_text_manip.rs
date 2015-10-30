17390a20a117b8e6963ecda96d5f5f8e58313902
Mika Attila <radiantstatue@gmail.com>
2015-10-30 12:33:25 +0100

    Make the text manipulation test be about text manipulation again

---
+++ src/bin/test_text_manip.rs
@@ -0,0 +1,11 @@
+extern crate rgmk;
+extern crate env_logger;
+
+fn main() {
+    env_logger::init().unwrap();
+    let path = std::env::args().skip(1).next().expect("Expected file name as argument");
+    let mut gamedata = rgmk::GameData::from_file(&path).unwrap();
+    /// Popping a char messes something up, the audio doesn't play.
+    gamedata.strings.strings[0].pop();
+    gamedata.save_to_file(&(path + ".rgmk")).unwrap();
+}
