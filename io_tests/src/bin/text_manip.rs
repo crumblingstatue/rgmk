d2569a7e488e150ad63ad53d9052ef3889adc00e
Mika Attila <radiantstatue@gmail.com>
2015-11-18 21:23:49 +0100

    Remove outdated information

--- io_tests/src/bin/text_manip.rs
+++ io_tests/src/bin/text_manip.rs
@@ -5,7 +5,6 @@ fn main() {
     env_logger::init().unwrap();
     let path = std::env::args().skip(1).next().expect("Expected file name as argument");
     let mut gamedata = rgmk::GameData::from_file(&path).unwrap();
-    // Popping a char messes something up, the audio doesn't play.
     gamedata.strings.strings[0].pop();
     gamedata.save_to_file(&(path + ".rgmk")).unwrap();
 }
