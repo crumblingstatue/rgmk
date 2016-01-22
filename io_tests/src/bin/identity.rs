022e8e5e1245735b031b4906b9e07b26852a6014
Mika Attila <radiantstatue@gmail.com>
2016-01-22 14:45:30 +0100

    io_tests/identity: print Display implementation of errors instead of Debug

--- io_tests/src/bin/identity.rs
+++ io_tests/src/bin/identity.rs
@@ -17,7 +17,7 @@ fn main() {
         vec
     };
     let game_data = rgmk::GameData::from_reader(&mut BufReader::new(Cursor::new(&original[..])))
-                        .unwrap();
+                        .unwrap_or_else(|e| panic!("Failed to read: {}", e));
     let mut new: Vec<u8> = vec![0; original.len()];
     game_data.write_to_writer(&mut BufWriter::new(Cursor::new(&mut new[..]))).unwrap();
     for (i, (o, n)) in original[..].iter().zip(new[..].iter()).enumerate() {
