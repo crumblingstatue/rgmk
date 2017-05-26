4b5f64e9a76ec7416c92e1f6a8f456179871235f
Mika Attila <radiantstatue@gmail.com>
2017-05-27 00:29:02 +0200

    Have chunks with concrete type and order instead of chunks of any type

--- src/pseudo_iff.rs
+++ src/pseudo_iff.rs
@@ -36,6 +36,10 @@ use {GameDataRead, GameDataWrite};
 //   24. (GLOB)
 //
 // - Chunks are aligned on even byte offsets. Padding bytes are always zero.
+//
+// - You can rely on the fact that chunks always have sizes (indicated by their
+//   IFF chunk size property) that land you on even alignment when reading,
+//   so you don't have to manually adjust alignment.
 
 /// A pseudo IFF-like format that Game Maker Studio uses.
 ///
@@ -47,34 +51,102 @@ use {GameDataRead, GameDataWrite};
 /// and errors in the handling of individual chunks don't break the rest of the chunks.
 #[derive(Debug)]
 pub struct PseudoIff {
-    pub chunks: Vec<Chunk>,
+    pub gen8: Vec<u8>,
+    pub optn: Vec<u8>,
+    pub extn: Vec<u8>,
+    pub sond: Vec<u8>,
+    pub agrp: Option<Vec<u8>>,
+    pub sprt: Vec<u8>,
+    pub bgnd: Vec<u8>,
+    pub path: Vec<u8>,
+    pub scpt: Vec<u8>,
+    pub shdr: Vec<u8>,
+    pub font: Vec<u8>,
+    pub tmln: Vec<u8>,
+    pub objt: Vec<u8>,
+    pub room: Vec<u8>,
+    pub dafl: Vec<u8>,
+    pub tpag: Vec<u8>,
+    pub code: Vec<u8>,
+    pub vari: Vec<u8>,
+    pub func: Vec<u8>,
+    pub strg: Vec<u8>,
+    pub txtr: Vec<u8>,
+    pub audo: Vec<u8>,
+    pub lang: Option<Vec<u8>>,
+    pub glob: Option<Vec<u8>>,
 }
-
-#[derive(Debug)]
-pub struct Chunk {
-    pub type_id: TypeId,
-    pub data: Vec<u8>,
-}
-
-type TypeId = [u8; 4];
 
 impl PseudoIff {
     pub fn load<R: GameDataRead>(reader: &mut R) -> Result<Self, Box<Error>> {
-        Ok(Self { chunks: read_form_chunk(reader)? })
+        read_form_chunk(reader)?;
+        Ok(Self {
+            gen8: read_opt_chunk(reader, b"GEN8")?.ok_or("missing GEN8 chunk")?,
+            optn: read_opt_chunk(reader, b"OPTN")?.ok_or("missing OPTN chunk")?,
+            extn: read_opt_chunk(reader, b"EXTN")?.ok_or("missing EXTN chunk")?,
+            sond: read_opt_chunk(reader, b"SOND")?.ok_or("missing SOND chunk")?,
+            agrp: read_opt_chunk(reader, b"AGRP")?,
+            sprt: read_opt_chunk(reader, b"SPRT")?.ok_or("missing SPRT chunk")?,
+            bgnd: read_opt_chunk(reader, b"BGND")?.ok_or("missing BGND chunk")?,
+            path: read_opt_chunk(reader, b"PATH")?.ok_or("missing PATH chunk")?,
+            scpt: read_opt_chunk(reader, b"SCPT")?.ok_or("missing SCPT chunk")?,
+            shdr: read_opt_chunk(reader, b"SHDR")?.ok_or("missing SHDR chunk")?,
+            font: read_opt_chunk(reader, b"FONT")?.ok_or("missing FONT chunk")?,
+            tmln: read_opt_chunk(reader, b"TMLN")?.ok_or("missing TMLN chunk")?,
+            objt: read_opt_chunk(reader, b"OBJT")?.ok_or("missing OBJT chunk")?,
+            room: read_opt_chunk(reader, b"ROOM")?.ok_or("missing ROOM chunk")?,
+            dafl: read_opt_chunk(reader, b"DAFL")?.ok_or("missing DAFL chunk")?,
+            tpag: read_opt_chunk(reader, b"TPAG")?.ok_or("missing TPAG chunk")?,
+            code: read_opt_chunk(reader, b"CODE")?.ok_or("missing CODE chunk")?,
+            vari: read_opt_chunk(reader, b"VARI")?.ok_or("missing VARI chunk")?,
+            func: read_opt_chunk(reader, b"FUNC")?.ok_or("missing FUNC chunk")?,
+            strg: read_opt_chunk(reader, b"STRG")?.ok_or("missing STRG chunk")?,
+            txtr: read_opt_chunk(reader, b"TXTR")?.ok_or("missing TXTR chunk")?,
+            audo: read_opt_chunk(reader, b"AUDO")?.ok_or("missing AUDO chunk")?,
+            lang: read_opt_chunk(reader, b"LANG")?,
+            glob: read_opt_chunk(reader, b"GLOB")?,
+        })
     }
     pub fn save<W: GameDataWrite>(&self, writer: &mut W) -> io::Result<()> {
         writer.write_all(b"FORM")?;
         // Skip writing the FORM length, because we don't know it yet.
         let after_form_len = writer.seek(SeekFrom::Current(4))?;
-        for chunk in &self.chunks {
-            writer.write_all(&chunk.type_id[..])?;
-            writer.write_i32::<LittleEndian>(chunk.data.len() as i32)?;
-            writer.write_all(&chunk.data)?;
-            // Chunks are aligned on even byte boundaries
-            let pos = writer.seek(SeekFrom::Current(0))?;
-            if pos % 2 != 0 {
-                writer.write_u8(0)?;
+        macro_rules! write {
+            ($field:expr, $id:expr) => {
+                writer.write_all($id)?;
+                writer.write_i32::<LittleEndian>($field.len() as i32)?;
+                writer.write_all(&$field)?;
             }
+        }
+        write!(self.gen8, b"GEN8");
+        write!(self.optn, b"OPTN");
+        write!(self.extn, b"EXTN");
+        write!(self.sond, b"SOND");
+        if let Some(ref data) = self.agrp {
+            write!(data, b"AGRP");
+        }
+        write!(self.sprt, b"SPRT");
+        write!(self.bgnd, b"BGND");
+        write!(self.path, b"PATH");
+        write!(self.scpt, b"SCPT");
+        write!(self.shdr, b"SHDR");
+        write!(self.font, b"FONT");
+        write!(self.tmln, b"TMLN");
+        write!(self.objt, b"OBJT");
+        write!(self.room, b"ROOM");
+        write!(self.dafl, b"DAFL");
+        write!(self.tpag, b"TPAG");
+        write!(self.code, b"CODE");
+        write!(self.vari, b"VARI");
+        write!(self.func, b"FUNC");
+        write!(self.strg, b"STRG");
+        write!(self.txtr, b"TXTR");
+        write!(self.audo, b"AUDO");
+        if let Some(ref data) = self.lang {
+            write!(data, b"LANG");
+        }
+        if let Some(ref data) = self.glob {
+            write!(data, b"GLOB");
         }
         let end = writer.seek(SeekFrom::Current(0))?;
         let form_len = end - after_form_len;
@@ -85,7 +157,7 @@ impl PseudoIff {
     }
 }
 
-fn read_form_chunk<R: GameDataRead>(reader: &mut R) -> Result<Vec<Chunk>, Box<Error>> {
+fn read_form_chunk<R: GameDataRead>(reader: &mut R) -> Result<(), Box<Error>> {
     let mut type_id = [0; 4];
     reader.read_exact(&mut type_id)?;
     let _len = reader.read_i32::<LittleEndian>();
@@ -95,30 +167,13 @@ fn read_form_chunk<R: GameDataRead>(reader: &mut R) -> Result<Vec<Chunk>, Box<Er
             "Invalid data.win file. Must start with a FORM chunk.".into(),
         );
     }
-
-    read_chunks(reader)
+    Ok(())
 }
 
-fn read_chunks<R: GameDataRead>(reader: &mut R) -> Result<Vec<Chunk>, Box<Error>> {
-    let mut chunks = Vec::new();
-
-    loop {
-        match read_chunk(reader) {
-            Ok(Some(chunk)) => {
-                // Chunks are aligned on even byte boundaries
-                let pos = reader.seek(SeekFrom::Current(0))?;
-                if pos % 2 != 0 {
-                    reader.seek(SeekFrom::Current(1))?;
-                }
-                chunks.push(chunk)
-            }
-            Ok(None) => return Ok(chunks),
-            Err(e) => return Err(e),
-        }
-    }
-}
-
-fn read_chunk<R: GameDataRead>(reader: &mut R) -> Result<Option<Chunk>, Box<Error>> {
+fn read_opt_chunk<R: GameDataRead>(
+    reader: &mut R,
+    expected_type: &'static [u8; 4],
+) -> Result<Option<Vec<u8>>, Box<Error>> {
     use std::io::ErrorKind;
 
     let mut type_id = [0; 4];
@@ -129,9 +184,15 @@ fn read_chunk<R: GameDataRead>(reader: &mut R) -> Result<Option<Chunk>, Box<Erro
         }
     }
 
+    if type_id != *expected_type {
+        // TODO: You can't just return None here, you already read the type id,
+        // Which consumed the type id of the next item you're about to read.
+        panic!("Optional chunks are not properly implemented.");
+    }
+
     let size = reader.read_i32::<LittleEndian>()?;
     let mut data = vec![0; size as usize];
     reader.read_exact(&mut data[..])?;
 
-    Ok(Some(Chunk { type_id, data }))
+    Ok(Some(data))
 }
